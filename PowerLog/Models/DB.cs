32df3cb9d7f6e9d39eb841f474c0c33c056725dd
Nikita <cshift3@simplevision.dk>
2012-09-25 15:48:51 +0200

    initial commit

---
+++ PowerLog/Models/DB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PowerLog.Models
+{
+    public class DB : DbContext
+    {
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ExerciseLog> Logs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ PowerLog/Models/Exercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PowerLog.Models
+{
+    public class Exercise
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ExerciseLog
+    {
+        [Key]
+        public int ID { get; set; }
+        public int ExerciseID { get; set; }
+        public virtual Exercise Exercise { get; set; }
+
+    [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public float Weight { get; set; }
+        public int Reps { get; set; }
+    }
+}
