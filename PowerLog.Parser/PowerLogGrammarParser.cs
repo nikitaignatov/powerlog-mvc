ffabba1c9ef70af92969c0ad06be6259ec4ac04b
Nikita <cshift3@simplevision.dk>
2012-10-12 00:19:35 +0200

    added a new Web project

--- PowerLog.Parser/PowerLogGrammarParser.cs
+++ PowerLog.Parser/PowerLogGrammarParser.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.4 C:\\Users\\Bacon\\Documents\\Visual Studio 2012\\Projects\\REPO\\PowerLog\\PowerLog.Parser\\PowerLogGrammar.g3 2012-10-11 23:25:38
+// $ANTLR 3.4 C:\\Users\\Bacon\\Documents\\Visual Studio 2012\\Projects\\REPO\\PowerLog\\PowerLog.Parser\\PowerLogGrammar.g3 2012-10-12 00:14:34
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
